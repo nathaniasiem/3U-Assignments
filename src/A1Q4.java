1efd6685b2013b8546adb849241b9ffdda93b309
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-02-09 10:14:07 -0500

    Modified A1Q4

--- src/A1Q4.java
+++ src/A1Q4.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -26,6 +27,12 @@ public class A1Q4 {
         
         //create another robot
         Robot Karel = new Robot (sentosa,0,1,Direction.SOUTH);
+        
+        // build barrier between robots
+        new Wall(sentosa,0,1,Direction.WEST);
+        new Wall(sentosa,1,1,Direction.WEST);
+        new Wall(sentosa,1,1,Direction.SOUTH);
+        
         
         //
         
